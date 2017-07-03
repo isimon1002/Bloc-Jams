8c8686a9d3aaec1dd62b18fc3edb69dd078a34dc
Isaac Simon <ijsimon@sbcglobal.net>
2017-07-03 18:14:40 -0500

    Checkpoint15

--- scripts/album.js
+++ scripts/album.js
@@ -149,9 +149,12 @@ window.onload = function() {
 
      songListContainer.addEventListener('mouseover', function(event) {
          if (event.target.parentElement.className === 'album-view-song-item') {
-             // Change the content from the number to the play button's HTML
-             event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
              var songItem = getSongItem(event.target);
+
+
+             if (songItem.getAttribute('data-song-number') !== currentlyPlayingSong) {
+                songItem.innerHTML = playButtonTemplate;
+            }
 
              }
          });
