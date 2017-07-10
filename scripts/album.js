bd250399e0dd21acaca68a3f8397201f3863d229
Isaac Simon <ijsimon@sbcglobal.net>
2017-07-10 14:46:36 -0500

    checkpoint20 assignment with problems

--- scripts/album.js
+++ scripts/album.js
@@ -1,3 +1,14 @@
+var togglePlayFromPlayerBar = function(){
+  if(currentSoundFile.isPaused()) {
+        $('.main-controls .play-pause').html(playerBarPauseButton);
+        currentSoundFile.play();
+        }
+  if(currentSoundFile.isPlay()){
+        $('.main-controls .play-pause').html(playerBarPlayButton);
+        currentSoundFile.pause();
+        }
+
+};
 var setSong = function(songNumber) {
     if(currentSoundFile){
       currentSoundFile.stop();
@@ -199,11 +210,13 @@ var currentVolume = 80;
 
 var $previousButton = $('.main-controls .previous');
 var $nextButton = $('.main-controls .next');
+var $playPauseButton = $('.main-controls .play-pause');
 
 $(document).ready(function() {
     setCurrentAlbum(albumPicasso);
     $previousButton.click(previousSong);
     $nextButton.click(nextSong);
+    $playPauseButton.click(togglePlayFromPlayerBar);
 });
      var album = [albumPicasso, albumMarconi, albumFavorite];
      var i = 0;
